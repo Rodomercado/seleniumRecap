6e4662901a2370cf82a7aa89e4abac28e44566cb
Rodomercado <65274543+Rodomercado@users.noreply.github.com>
2022-08-04 19:48:40 -0300

    Add files via upload

---
+++ pageitem.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+import unittest
+
+class Page_item:
+    def __init__(self, driver):
+        self.driver = driver
+        self.title = (By.XPATH, '//h1[@itemprop="name"]')
+
+    def verify_text(self, text_to_verify):
+        my_assertion = unittest.TestCase('__init__')
+        #title = self.driver.find_element(By.XPATH, '//h1[@itemprop="name"]').text
+        title = self.driver.find_element(*self.title).text
+        #self.assertEqual(title, text_to_verify, 'Text should be different')
+        my_assertion.assertEqual(title, text_to_verify, 'Text should be different')
