024040db98bb56179c72157ddbac9c314be74de8
Rodomercado <65274543+Rodomercado@users.noreply.github.com>
2022-08-04 12:28:50 -0300

    Add files via upload

--- ecommerceV2.py
+++ ecommerceV2.py
@@ -11,18 +11,16 @@ class Items(unittest.TestCase):
     #    pass
 
     def test_view_item_page(self):
-        self.driver.find_element(By.ID, "search_query_top").send_keys('dress')
+        self.driver.find_element(By.ID, "search_query_top").send_keys('dress')  #encontrar elementos
         self.driver.find_element(By.NAME, "submit_search").click()
-        self.driver.implicitly_wait(5)
+        self.driver.implicitly_wait(5)  #detiene ejecucion por 5 segundos
         self.driver.find_element(By.XPATH, '//*[@id="center_column"]/ul/li[1]/div/div[1]/div/a[1]/img').click()
         title = self.driver.find_element(By.XPATH, '//h1[@itemprop="name"]').text
         #self.assertEqual(title, 'otra cosa', 'Text should be different')
-        self.assertEqual(title, 'Printed Summer Dress', 'Text should be different')
+        self.assertEqual(title, 'Printed Summer Dress', 'Text should be different') #verifica las variables
 
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
-
-
--- ecommerceV4.py
+++ ecommerceV4.py
@@ -1,28 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import unittest
+from pages.pageindex import *
 
 class Items(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome('chromedriver.exe')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--incognito') #agregar argumento, iniciar en modo incognito
+        #chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome('chromedriver.exe', options = chrome_options)
         self.driver.get('http://automationpractice.com/index.php')
 
     #def test_do_nothing(self):
     #    pass
 
     def test_view_item_page(self):
-        self.driver.find_element(By.ID, "search_query_top").send_keys('dress')
-        self.driver.find_element(By.NAME, "submit_search").click()
+        page_index = Page_index(self.driver)
+        page_index.search_items('dress')
         self.driver.implicitly_wait(5)
         self.driver.find_element(By.XPATH, '//*[@id="center_column"]/ul/li[1]/div/div[1]/div/a[1]/img').click()
         title = self.driver.find_element(By.XPATH, '//h1[@itemprop="name"]').text
         #self.assertEqual(title, 'otra cosa', 'Text should be different')
         self.assertEqual(title, 'Printed Summer Dress', 'Text should be different')
 
+    def test_search_with_no_items(self):
+        page_index = Page_index(self.driver)
+        page_index.search_items('computer')
+
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
-
-
